b22818f86cc8e08cb4adc659e06d21e38311f67a
LiudmilaBayik <25224716-LiudmilaBayik@users.noreply.replit.com>
2023-08-15 11:37:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function main() {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.canvas.width = 300;
+  ctx.canvas.height = 300;
+  // Start a new Path
+  ctx.beginPath();
+
+  ctx.moveTo(0, 0);
+  ctx.lineTo(300, 150);
+  // Draw the Path
+  ctx.stroke();
+
+  drawCircle(2);
+}
+
+function drawCircle(radius) {
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+  // Start a new Path
+  ctx.beginPath();
+
+  for (var i = 0; i < Math.PI * 60 + 0.1; i += 0.01) {
+    var x = 150 + Math.cos(i) * radius;
+    var y = 150 + Math.sin(i) * radius;
+    radius *= 1.0007;
+    if (i == 0) {
+      ctx.moveTo(x, y);
+    }
+    else {
+      ctx.lineTo(x, y);
+    }
+
+  }
+
+  ctx.stroke();
+}
